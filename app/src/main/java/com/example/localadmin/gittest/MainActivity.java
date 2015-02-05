e055605ea1fbc44d57f821a4c56bd257ca34261e
RegardtS <regardt@myself.com>
2015-02-05 10:40:29 +0200

    Added import

--- app/src/main/java/com/example/localadmin/gittest/MainActivity.java
+++ app/src/main/java/com/example/localadmin/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.localadmin.gittest;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,7 +14,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("foobar","Kneel before zod!");
+        Log.d("foobar", "Kneel before zod!");
+
     }
 
 
